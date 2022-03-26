46a5bf0b7ecb3f56e0e8f3c3af670cc5e494922d
aiden <zero2one23581@gmail.com>
2022-03-27 02:40:01 +0900

    ⚡️ React Hook Form - validation

--- pages/forms.tsx
+++ pages/forms.tsx
@@ -1,13 +1,28 @@
 import { useForm } from "react-hook-form";
 
 export default function Forms() {
-  const { register, watch } = useForm();
-  console.log(watch());
+  const { register, handleSubmit } = useForm();
+  const onValid = () => {
+    console.log(`VALID!!`);
+  };
+
   return (
-    <form>
-      <input {...register("username")} required type="text" placeholder="Username" />
-      <input {...register("email")} required type="email" placeholder="Email" />
-      <input {...register("password")} required type="password" placeholder="Password" />
+    <form onSubmit={handleSubmit(onValid)}>
+      <input
+        {...register("username", { required: true })}
+        type="text"
+        placeholder="Username"
+      />
+      <input
+        {...register("email", { required: true })}
+        type="email"
+        placeholder="Email"
+      />
+      <input
+        {...register("password", { required: true })}
+        type="password"
+        placeholder="Password"
+      />
       <input type="submit" value="Create Account" />
     </form>
   );
