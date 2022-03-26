a709a759d7d994f5eeffd78e70a1c8da4a39a4b3
aiden <zero2one23581@gmail.com>
2022-03-27 03:22:07 +0900

    ⚡️  React Hook Form - validation 3

--- pages/forms.tsx
+++ pages/forms.tsx
@@ -7,13 +7,20 @@ interface LoginForm {
 }
 
 export default function Forms() {
-  const { register, handleSubmit } = useForm<LoginForm>();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginForm>({ mode: "onChange" });
+
   const onValid = (data: LoginForm) => {
     console.log(`VALID!!`);
   };
   const onInValid = (errors: FieldErrors) => {
-    console.log(errors);
+    // console.log(errors);
   };
+
+  // console.log(errors);
 
   return (
     <form onSubmit={handleSubmit(onValid, onInValid)}>
@@ -27,17 +34,29 @@ export default function Forms() {
         })}
         type="text"
         placeholder="Username"
+        className={`${Boolean(errors.username) ? "outline-red-500" : ""}`}
       />
+      {errors.username?.message}
       <input
-        {...register("email", { required: "EMAIL IS REQUIRED!" })}
+        {...register("email", {
+          required: "EMAIL IS REQUIRED!",
+          validate: {
+            gmailRestrict: (value) =>
+              !value.includes("@gmail.com") || "GMAIL IS NOW ALLOWED!",
+          },
+        })}
         type="email"
         placeholder="Email"
+        className={`${Boolean(errors.email) ? "outline-red-500" : ""}`}
       />
+      {errors.email?.message}
       <input
         {...register("password", { required: "PASSWORD IS REQUIRED!" })}
         type="password"
         placeholder="Password"
+        className={`${Boolean(errors.password) ? "outline-red-500" : ""}`}
       />
+      {errors.password?.message}
       <input type="submit" value="Create Account" />
     </form>
   );
