ef9832f486278a195361b4345ecd3c9cb0d7b9d1
aiden <zero2one23581@gmail.com>
2022-03-27 02:50:27 +0900

    ⚡️ React Hook Form - validation 2

--- pages/forms.tsx
+++ pages/forms.tsx
@@ -1,25 +1,40 @@
-import { useForm } from "react-hook-form";
+import { FieldErrors, useForm } from "react-hook-form";
+
+interface LoginForm {
+  username: string;
+  password: string;
+  email: string;
+}
 
 export default function Forms() {
-  const { register, handleSubmit } = useForm();
-  const onValid = () => {
+  const { register, handleSubmit } = useForm<LoginForm>();
+  const onValid = (data: LoginForm) => {
     console.log(`VALID!!`);
+  };
+  const onInValid = (errors: FieldErrors) => {
+    console.log(errors);
   };
 
   return (
-    <form onSubmit={handleSubmit(onValid)}>
+    <form onSubmit={handleSubmit(onValid, onInValid)}>
       <input
-        {...register("username", { required: true })}
+        {...register("username", {
+          required: "USERNAME IS REQUIRED!",
+          minLength: {
+            value: 5,
+            message: "USERNAME SHOULD BE LONGER THAN 5!",
+          },
+        })}
         type="text"
         placeholder="Username"
       />
       <input
-        {...register("email", { required: true })}
+        {...register("email", { required: "EMAIL IS REQUIRED!" })}
         type="email"
         placeholder="Email"
       />
       <input
-        {...register("password", { required: true })}
+        {...register("password", { required: "PASSWORD IS REQUIRED!" })}
         type="password"
         placeholder="Password"
       />
