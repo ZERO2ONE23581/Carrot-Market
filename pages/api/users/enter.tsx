1489b40aa2da75d0f812f0b814aa5c90b21a34be
aiden <zero2one23581@gmail.com>
2022-03-27 14:27:02 +0900

    ♻️ Accounts Logic - Upsert 2

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -4,21 +4,18 @@ import client from "@libs/server/client";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { email, phone } = req.body;
-
   //### UPSERT를 활용한 리팩토링 ###
 
+  //1. 이메일(번호)을 입력했다면 ? => 해당하는 user를 찾는다.
+  const payload = email ? { email } : { phone: +phone };
   const user = await client.user.upsert({
-    //1-1. 입력한 이메일이 있다면 ? => 해당하는 메일의 user를 찾는다.
-    //1-2. 입력한 번호가 있다면 ? => 해당하는 번호의 user를 찾는다.
     where: {
-      ...(email ? { email } : {}),
-      ...(phone ? { phone: +phone } : {}),
+      ...payload,
     },
-    //2. 없다면 입력값으로 새로운 user 데이터 생성
+    //2. 해당 user 없다면 입력값으로 새로운 user 데이터 생성
     create: {
       name: "Anonymous",
-      ...(email ? { email } : {}),
-      ...(phone ? { phone: +phone } : {}),
+      ...payload,
     },
     //3. 업데이트는 안함. (upsert사용시 필수적으로 코드 써줘야함)
     update: {},
