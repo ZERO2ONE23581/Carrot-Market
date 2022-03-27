874cd4aabba28167621ecdb7757327defa8e9f7e
aiden <zero2one23581@gmail.com>
2022-03-27 20:56:04 +0900

    ✨ Serverless Sessions

--- libs/server/withHandler.ts
+++ libs/server/withHandler.ts
@@ -10,7 +10,7 @@ export default function withHandler(
   fn: (req: NextApiRequest, res: NextApiResponse) => void
 ) {
   //이부분이 next가 실행하는 return 함수!!
-  return async function (req: NextApiRequest, res: NextApiResponse) {
+  return async function (req: NextApiRequest, res: NextApiResponse): Promise<any> {
     // 우리가 원하는 method가 아니면 연결끊음
     if (req.method !== method) {
       return res.status(405).end();
--- pages/api/users/confirm.tsx
+++ pages/api/users/confirm.tsx
@@ -1,10 +1,25 @@
+import { withIronSessionApiRoute } from "iron-session/next";
 import { NextApiRequest, NextApiResponse } from "next";
 import withHandler, { ResponseType } from "@libs/server/withHandler";
+import client from "@libs/server/client";
+import { execSync } from "child_process";
 
 async function handler(req: NextApiRequest, res: NextApiResponse<ResponseType>) {
   const { token } = req.body;
-  console.log(token);
+  const exists = await client.token.findUnique({
+    where: { payload: token },
+    include: { user: true },
+  });
+  if (!exists) res.status(404).end();
+  console.log(exists);
+  req.session.user = {
+    id: exists?.userId,
+  };
+  await req.session.save();
   res.status(200).end();
 }
 
-export default withHandler("POST", handler);
+export default withIronSessionApiRoute(withHandler("POST", handler), {
+  cookieName: "carrotSession",
+  password: "102934812304asdklfj;alsdkfja;sdklfjaskl;dfj981023948",
+});
