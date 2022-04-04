83f4b7b993557294311238d5f2273efaf5f78987
aiden <zero2one23581@gmail.com>
2022-04-04 09:28:08 +0900

    ♻️ useUser refactoring

--- libs/client/userUser.ts
+++ libs/client/userUser.ts
@@ -2,13 +2,16 @@ import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import useSWR from "swr";
 
-//fetch function fetches(brings) the data and return
-//데이터가 swr캐시에 저장된다. 페이지를 이동할때 undefined가 뜨지 않는다.
-//swr의 url 자체가 id가 되어 hook을 어디에서나 쓸수있도록 해준다.
-const fetcher = (url: string) => fetch(url).then((response) => response.json());
-
 export default function useUser() {
-  const { data, error } = useSWR("/api/users/me", fetcher);
+  const { data, error } = useSWR("/api/users/me");
 
-  return data;
+  const router = useRouter();
+  useEffect(() => {
+    //로그인이 되지 않았다면 enter페이지로 이동
+    if (data && !data.ok) {
+      router.replace("/enter");
+    }
+  }, [data, router]);
+
+  return { user: data?.profile, isLoading: !data && !error };
 }
