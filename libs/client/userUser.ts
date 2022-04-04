dec0ab82c757f2895df8123fea8f127e9fccde47
aiden <zero2one23581@gmail.com>
2022-04-04 09:13:31 +0900

    ✨ SWR

--- libs/client/userUser.ts
+++ libs/client/userUser.ts
@@ -1,20 +1,14 @@
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
+import useSWR from "swr";
+
+//fetch function fetches(brings) the data and return
+//데이터가 swr캐시에 저장된다. 페이지를 이동할때 undefined가 뜨지 않는다.
+//swr의 url 자체가 id가 되어 hook을 어디에서나 쓸수있도록 해준다.
+const fetcher = (url: string) => fetch(url).then((response) => response.json());
 
 export default function useUser() {
-  const [user, setUser] = useState();
-  const router = useRouter();
-  useEffect(() => {
-    fetch("/api/users/me")
-      .then((response) => response.json())
-      .then((data) => {
-        //
-        if (!data.ok) {
-          return router.replace("/enter");
-          //router.push는 browser에 history가 남고, replace는 남지 않는다.
-        }
-        setUser(data.profile);
-      });
-  }, []);
-  return user;
+  const { data, error } = useSWR("/api/users/me", fetcher);
+
+  return data;
 }
