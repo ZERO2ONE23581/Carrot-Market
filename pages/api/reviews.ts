3f68308bbbd0d63118c32f17e7490c9854866bd4
aiden <zero2one23581@gmail.com>
2022-04-20 10:24:59 +0900

    #13.1 Reviews

---
+++ pages/api/reviews.ts
@@ -0,0 +1,33 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import withHandler, { ResponseType } from '@libs/server/withHandler';
+import client from '@libs/server/client';
+import { withApiSession } from '@libs/server/withSession';
+
+//For ONLY logged in user!
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
+  const { user } = req.session;
+
+  //나한테 달린 리뷰찾기
+  const reviews = await client.review.findMany({
+    where: {
+      createdForId: user?.id,
+    },
+    include: {
+      createBy: {
+        select: {
+          id: true,
+          name: true,
+          avatar: true,
+        },
+      },
+    },
+  });
+
+  //
+  return res.json({ ok: true, reviews });
+}
+
+export default withApiSession(withHandler({ methods: ['GET'], handler }));
