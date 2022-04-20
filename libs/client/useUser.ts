276854318c9972f22159a7d4c6d9f1365a123c39
aiden <zero2one23581@gmail.com>
2022-04-20 12:24:27 +0900

    #13.5 Edit Profile part1

--- libs/client/useUser.ts
+++ libs/client/useUser.ts
@@ -1,15 +1,21 @@
-import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
-import useSWR from "swr";
+import { User } from '@prisma/client';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import useSWR from 'swr';
+
+interface ProfileResponse {
+  ok: boolean;
+  profile: User;
+}
 
 export default function useUser() {
-  const { data, error } = useSWR("/api/users/me");
+  const { data, error } = useSWR<ProfileResponse>('/api/users/me');
 
   const router = useRouter();
   useEffect(() => {
     //로그인이 되지 않았다면 enter페이지로 이동
     if (data && !data.ok) {
-      router.replace("/enter");
+      router.replace('/enter');
     }
   }, [data, router]);
 
--- pages/profile/edit.tsx
+++ pages/profile/edit.tsx
@@ -1,12 +1,42 @@
-import type { NextPage } from "next";
-import Button from "@components/button";
-import Input from "@components/input";
-import Layout from "@components/layout";
+import type { NextPage } from 'next';
+import Button from '@components/button';
+import Input from '@components/input';
+import Layout from '@components/layout';
+import useUser from '@libs/client/useUser';
+import { useForm } from 'react-hook-form';
+import { useEffect } from 'react';
+
+interface EditProfileForm {
+  email?: string;
+  phone?: string;
+  formErrors?: string;
+}
 
 const EditProfile: NextPage = () => {
+  const { user } = useUser();
+  const {
+    register,
+    setValue,
+    handleSubmit,
+    setError,
+    formState: { errors },
+  } = useForm<EditProfileForm>();
+
+  //초기값 설정
+  useEffect(() => {
+    if (user?.email) setValue('email', user.email);
+    if (user?.phone) setValue('phone', user.phone);
+  }, [user, setValue]);
+  //
+  const onValid = ({ email, phone }: EditProfileForm) => {
+    if (email === '' && phone === '') {
+      setError('formErrors', { message: 'Email or Phone Number is required!' });
+    }
+  };
+  //
   return (
     <Layout canGoBack title="Edit Profile">
-      <form className="py-10 px-4 space-y-4">
+      <form onSubmit={handleSubmit(onValid)} className="py-10 px-4 space-y-4">
         <div className="flex items-center space-x-3">
           <div className="w-14 h-14 rounded-full bg-slate-500" />
           <label
@@ -14,11 +44,34 @@ const EditProfile: NextPage = () => {
             className="cursor-pointer py-2 px-3 border hover:bg-gray-50 border-gray-300 rounded-md shadow-sm text-sm font-medium focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 text-gray-700"
           >
             Change
-            <input id="picture" type="file" className="hidden" accept="image/*" />
+            <input
+              id="picture"
+              type="file"
+              className="hidden"
+              accept="image/*"
+            />
           </label>
         </div>
-        <Input required label="Email address" name="email" type="email" />
-        <Input required label="Phone number" name="phone" type="number" kind="phone" />
+        <Input
+          register={register('email')}
+          required={false}
+          label="Email address"
+          name="email"
+          type="email"
+        />
+        <Input
+          register={register('phone')}
+          required={false}
+          label="Phone number"
+          name="phone"
+          type="number"
+          kind="phone"
+        />
+        {errors.formErrors ? (
+          <span className="my-2 text-red-600 font-bold block text-center">
+            {errors.formErrors.message}
+          </span>
+        ) : null}
         <Button text="Update profile" />
       </form>
     </Layout>
